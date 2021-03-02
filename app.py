a2e0a6be864c1bbd38a912aea9afec0e35c4c464
DanielKorsa <gesundmeister@gmail.com>
2021-03-02 17:03:08 +0100

    runnin on local network

--- app.py
+++ app.py
@@ -1,81 +1,47 @@
-#
-#import quandl
 import dash
-import dash_table
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-
-#from pandas_datareader import data as web
 import pandas as pd
-from pandas_datareader import data as pdr
-import datetime as dt
-import yfinance as yf
-yf.pdr_override()#! Connector from pandas_datareader to Yahoo Finance
+import plotly.graph_objs as go
 
-user_stonks_df = pd.read_csv('my_stonks.csv')
+app = dash.Dash()
 
-today = dt.datetime.now()
-week = today - dt.timedelta(days=7)
-span_7_days = week.replace(hour=0, minute=0, second=0, microsecond=0)
-month = today - dt.timedelta(days=30)
-span_30_days = month.replace(hour=0, minute=0, second=0, microsecond=0)
-year = today - dt.timedelta(days=365)
-span_365_days = year.replace(hour=0, minute=0, second=0, microsecond=0)
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
 
 
-
-app = dash.Dash('Stonks Dashboard')
-
-app.layout = html.Div(children=[
-    html.H1(children='Stonks Monitor'),
-
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'Blizzard', 'value': 'ACTIVISION BLIZZARD'},
-            {'label': 'Alibaba', 'value': 'ALIBABA'},
-            {'label': 'Nio', 'value': 'NIO'},
-            {'label': 'Apple', 'value': 'AAPL'}
-        ],
-        value='NIO'
-    ),
-    dcc.Graph(id='my-graph'),
-    html.Div(children='''
-        Time Span
-    '''),
-    dcc.RadioItems(
-        options=[
-            {'label': 'Day', 'value': 'day'},
-            {'label': 'Week', 'value': 'week'},
-            {'label': 'Month', 'value': 'month'}
-        ],
-        value='day',
-        labelStyle={'display': 'inline-block'},
-        style={'padding':'300'}
-    ),
-    dash_table.DataTable(
-    id='stonk_table',
-    columns=[{"name": i, "id": i} for i in user_stonks_df.columns],
-    data=user_stonks_df.to_dict('records'),
-)
-], style={'width': '200'}
-)
-
-@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
-def update_graph(selected_dropdown_value):
-    df = pdr.get_data_yahoo(selected_dropdown_value, start=span_7_days, end=today)
-
-
-    return {
-        'data': [{
-            'x': df.index,
-            'y': df.Close
-        }],
-        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
-    }
-
-app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.8,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    #app.run_server()
+    app.run_server(host= '0.0.0.0', port=8050,debug=False)
--- test.py
+++ test.py
@@ -1,96 +1,96 @@
-#
-import pprint
-import subprocess
+# #
+# import pprint
+# import subprocess
 
-from collections import Counter
-
-
-#! Files opened by user
-# p4 opened -u  kilian.fröhler
-#p4 opened -u  danil.konowalow
-
-#! Files opened by folder-------------------
-
-#! p4 opened -a //Flintstone/main/...
-#! -----------------------------------------
-
-p4_nanion_team = [
-                'danil.konowalow',
-                'kilian.fröhler',
-                # 'peter.prinzen',
-                # 'johannes.stiehler',
-                # 'igor.pugliesi'
-]
+# from collections import Counter
 
 
-def run_command(command):
-    p = subprocess.Popen(command,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.STDOUT)
-    return p.stdout.read()
+# #! Files opened by user
+# # p4 opened -u  kilian.fröhler
+# #p4 opened -u  danil.konowalow
+
+# #! Files opened by folder-------------------
+
+# #! p4 opened -a //Flintstone/main/...
+# #! -----------------------------------------
+
+# p4_nanion_team = [
+#                 'danil.konowalow',
+#                 'kilian.fröhler',
+#                 # 'peter.prinzen',
+#                 # 'johannes.stiehler',
+#                 # 'igor.pugliesi'
+# ]
 
 
-def checkedout_files_user(user_name):
-
-    file_list = run_command('p4 opened -u ' + user_name)
-
-    return file_list
-
-def checkedout_files_depot(depot_name):
-
-    cmd_out = run_command('p4 opened -a ' + depot_name)
-    #file_list = str(cmd_out)
-    file_list = str(cmd_out, encoding="utf-8", errors='ignore')
-    #print(type(cmd_out))
-    #file_list.splitlines()
-    file_list = file_list.split('\r\n')
-
-    return file_list
+# def run_command(command):
+#     p = subprocess.Popen(command,
+#                         stdout=subprocess.PIPE,
+#                         stderr=subprocess.STDOUT)
+#     return p.stdout.read()
 
 
-# reply = run_command('p4 sync')
-# print(reply)
+# def checkedout_files_user(user_name):
 
-# for user in p4_nanion_team:
-#     file_list = str(checkedout_files_user(user))
-#     #print(type(file_list))
-#     pprint.pprint(file_list)
+#     file_list = run_command('p4 opened -u ' + user_name)
 
-chekedout_files_info = checkedout_files_depot('//Flintstone/main/...')
+#     return file_list
 
-checkedout_files_d = []
-user_list = []
-files_list = []
+# def checkedout_files_depot(depot_name):
 
-for co_file in chekedout_files_info:
+#     cmd_out = run_command('p4 opened -a ' + depot_name)
+#     #file_list = str(cmd_out)
+#     file_list = str(cmd_out, encoding="utf-8", errors='ignore')
+#     #print(type(cmd_out))
+#     #file_list.splitlines()
+#     file_list = file_list.split('\r\n')
 
-    info = {}
-    try:
-        f_path, user = co_file.split('#')
-        user = user.split('by')[1].split('@')[0]
-        info['path'] = f_path
-        info['user'] = user
-        user_list.append(user)
-        files_list.append(f_path)
-    except ValueError:
-        pass
-
-    checkedout_files_d.append(info)
-
-print('There are {} currently checked out'.format(len(files_list)))
-
-#! Checked out data stats by user
-checkedout_stats = Counter(user_list)
-pprint.pprint(checkedout_stats)
-
-#duplicates = list(set(files_list))
-
-#! Cross checked out files
-duplicates = set([x for x in files_list if files_list.count(x) > 1])
-pprint.pprint(duplicates)
-print(len(duplicates))
-#print(potential_issue)
-#print(type(potential_issue))
+#     return file_list
 
 
-'//Flintstone/main/WP_based/Panels/AnalyserPanel2Y/Private/GraphicObjects/IC50-Control/AnaPnl_PX_GraphicObject_Handler(IC50-Control).vi#49 - edit default change (binary) by peter.prinzen@peter.prinzen_Arco2_305\r\n'
+# # reply = run_command('p4 sync')
+# # print(reply)
+
+# # for user in p4_nanion_team:
+# #     file_list = str(checkedout_files_user(user))
+# #     #print(type(file_list))
+# #     pprint.pprint(file_list)
+
+# chekedout_files_info = checkedout_files_depot('//Flintstone/main/...')
+
+# checkedout_files_d = []
+# user_list = []
+# files_list = []
+
+# for co_file in chekedout_files_info:
+
+#     info = {}
+#     try:
+#         f_path, user = co_file.split('#')
+#         user = user.split('by')[1].split('@')[0]
+#         info['path'] = f_path
+#         info['user'] = user
+#         user_list.append(user)
+#         files_list.append(f_path)
+#     except ValueError:
+#         pass
+
+#     checkedout_files_d.append(info)
+
+# print('There are {} currently checked out'.format(len(files_list)))
+
+# #! Checked out data stats by user
+# checkedout_stats = Counter(user_list)
+# pprint.pprint(checkedout_stats)
+
+# #duplicates = list(set(files_list))
+
+# #! Cross checked out files
+# duplicates = set([x for x in files_list if files_list.count(x) > 1])
+# pprint.pprint(duplicates)
+# print(len(duplicates))
+# #print(potential_issue)
+# #print(type(potential_issue))
+
+
+# '//Flintstone/main/WP_based/Panels/AnalyserPanel2Y/Private/GraphicObjects/IC50-Control/AnaPnl_PX_GraphicObject_Handler(IC50-Control).vi#49 - edit default change (binary) by peter.prinzen@peter.prinzen_Arco2_305\r\n'
